ab65ae0a15b9956fcc6331aa09e242a530e648f9
mrzhenshen <mrzhenshen@gmail.com>
2023-10-25 10:46:36 +0300

    done oop hw create Button class refactor init of Demo related classes create Handler for execution

---
+++ src/main/java/oop/Handler.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Handler {
+  public static void main(String[] args) {
+    // Task 1
+    System.out.println(new Button());
+
+    // Task 2
+    new Demo().main();
+  }
+}
